3256135c10ef3ba5c625c4169ddb4f3984d805f0
Gavin Johnson <gjohn140@students.kennesaw.edu>
2023-04-10 21:12:25 -0400

    took away config

--- Sign-In/app.js
+++ Sign-In/app.js
@@ -1,3 +1,9 @@
+// Initialize Firebase
+
+const app = initializeApp(firebaseConfig);
+
+const analytics = getAnalytics(app);
+
 const form = document.querySelector("form");
 form.addEventListener("submit", function (event) {
   event.preventDefault();
@@ -10,37 +16,3 @@ form.addEventListener("submit", function (event) {
   form.submit();
 });
 // Import the functions you need from the SDKs you need
-
-import { initializeApp } from "firebase/app";
-
-import { getAnalytics } from "firebase/analytics";
-
-// TODO: Add SDKs for Firebase products that you want to use
-
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-
-const firebaseConfig = {
-  apiKey: "AIzaSyB96o6uX7t56qM_fbYlbiEjzP7IXHHBqyc",
-
-  authDomain: "twitter-clone-official.firebaseapp.com",
-
-  projectId: "twitter-clone-official",
-
-  storageBucket: "twitter-clone-official.appspot.com",
-
-  messagingSenderId: "574757579990",
-
-  appId: "1:574757579990:web:ac501b607f0231ae452d77",
-
-  measurementId: "G-YEPC94PGFJ",
-};
-
-// Initialize Firebase
-
-const app = initializeApp(firebaseConfig);
-
-const analytics = getAnalytics(app);
