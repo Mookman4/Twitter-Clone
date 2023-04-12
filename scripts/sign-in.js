5aa032b372f3e9ed14cafc5d3c0d1486b3d6b23b
Gavin Johnson <gjohn140@students.kennesaw.edu>
2023-04-12 18:37:12 -0400

    changed the css for the sign-in page

--- scripts/sign-in.js
+++ scripts/sign-in.js
@@ -1,44 +1,29 @@
 let fireBaseURL = "https://twitter-clone-official-default-rtdb.firebaseio.com/";
-let jsonEXT = ".json"; // .json is the extension for firebase
+let jsonEXT = ".json"; // .json is the extension for Firebase
 
-document.addEventListener("DOMContentLoaded", () => {
-  const fullname = document.getElementById("name");
-  const username = document.getElementById("username");
-  const email = document.getElementById("email");
-  const password = document.getElementById("password");
-  const loginBtn = document.getElementById("loginBtn");
+document.addEventListener("DOMContentLoaded", function () {
+  const usernameInput = document.getElementById("username");
+  const passwordInput = document.getElementById("password");
+  const loginButton = document.querySelector("button[type='submit']");
 
-  loginBtn.addEventListener("click", function (event) {
+  loginButton.addEventListener("click", function (event) {
     event.preventDefault();
 
-    if (
-      fullname.value === "" ||
-      username.value === "" ||
-      email.value === "" ||
-      password.value === ""
-    ) {
-      alert("Please fill all the fields");
-    } else {
-      fetch(`${fireBaseURL}Users/${jsonEXT}`, {
-        method: "POST",
-        body: JSON.stringify({
-          fullname: fullname.value,
-          username: username.value,
-          email: email.value,
-          password: password.value,
-        }),
+    let username = usernameInput.value;
+    let password = passwordInput.value;
+
+    fetch(`${fireBaseURL}Users/${jsonEXT}`, {
+      method: "POST",
+      body: JSON.stringify(username),
+    })
+      .then((res) => {
+        if (user.password === password) {
+          // Login successful, redirect to home page
+          window.location.href = "http://127.0.0.1:5500/Main-Feed/index.html";
+        } else {
+          alert("Incorrect username or password. Please try again.");
+        }
       })
-        .then((res) => res.json())
-        .then((data) => {
-          console.log(data);
-        });
-    }
-
-    fullname.value = "";
-    username.value = "";
-    email.value = "";
-    password.value = "";
-
-    window.location.href = "http://127.0.0.1:5500/Main-Feed/index.html";
+      .catch((err) => console.log(err));
   });
 });
