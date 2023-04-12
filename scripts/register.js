39aadab53f599c131819c98dfecc7c0abab03eeb
Gavin Johnson <gjohn140@students.kennesaw.edu>
2023-04-12 16:59:13 -0400

    Made the forms into an entire event listerner

--- scripts/register.js
+++ scripts/register.js
@@ -1,48 +1,39 @@
 let fireBaseURL = "https://twitter-clone-official-default-rtdb.firebaseio.com/";
 let jsonEXT = ".json"; // .json is the extension for Firebase
 
-function getData() {
-  fetch(`${fireBaseURL}users${jsonEXT}`)
-    .then((res) => res.json())
-    .then((data) => console.log(data))
-    .catch((err) => console.log(err));
-}
+// Wait for the HTML to finish loading
+document.addEventListener("DOMContentLoaded", function () {
+  // Get the form elements defined in your form HTML above
 
-// Get the HTML elements for the form inputs and submit button
-const fullNameInput = document.getElementById("name");
-const usernameInput = document.getElementById("username");
-const emailInput = document.getElementById("email");
-const passwordInput = document.getElementById("password");
-const registerButton = document.querySelector("button[type='submit']");
+  const fullNameInput = document.getElementById("name");
+  const usernameInput = document.getElementById("username");
+  const emailInput = document.getElementById("email");
+  const passwordInput = document.getElementById("password");
+  const registerButton = document.getElementById("submit");
 
-// Add an event listener for when the register button is clicked
-registerButton.addEventListener("click", function (event) {
-  // Prevent the default form submission behavior
-  event.preventDefault();
+  registerButton.addEventListener("click", function (event) {
+    event.preventDefault();
 
-  // Get the values of the form inputs
-  const fullName = fullNameInput.value;
-  const username = usernameInput.value;
-  const email = emailInput.value;
-  const password = passwordInput.value;
+    const userData = {
+      fullName: fullNameInput.value,
+      username: usernameInput.value,
+      email: emailInput.value,
+      password: passwordInput.value,
+    };
 
-  // Create an object with the user data
-  const userData = {
-    fullName: fullName,
-    username: username,
-    email: email,
-    password: password,
-  };
+    fetch(`${fireBaseURL}Users/${jsonEXT}`, {
+      method: "POST",
+      body: JSON.stringify(userData),
+    })
+      .then((res) => {
+        // Reset the form inputs
+        fullNameInput.value = "";
+        usernameInput.value = "";
+        emailInput.value = "";
+        passwordInput.value = "";
 
-  // Reset the form inputs
-  fullNameInput.value = "";
-  usernameInput.value = "";
-  emailInput.value = "";
-  passwordInput.value = "";
-
-  // Call the getData function to get the data from Firebase
-  getData();
-
-  // Redirect the user to the login page
-  window.location.href = "http://127.0.0.1:5500/Sign-In/";
+        window.location.href = "http://127.0.0.1:5500/Sign-In/";
+      })
+      .catch((err) => console.log(err));
+  });
 });
