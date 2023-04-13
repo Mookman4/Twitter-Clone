f1e3b7f1b04ddeeb26ec899c8b64f647a63a44a2
Gavin Johnson <gjohn140@students.kennesaw.edu>
2023-04-12 21:47:23 -0400

    modified the code that allows the user to sign in

--- scripts/sign-in.js
+++ scripts/sign-in.js
@@ -1,5 +1,6 @@
-let fireBaseURL = "https://twitter-clone-official-default-rtdb.firebaseio.com/";
-let jsonEXT = ".json"; // .json is the extension for Firebase
+const fireBaseURL =
+  "https://twitter-clone-official-default-rtdb.firebaseio.com/";
+const jsonEXT = ".json"; // .json is the extension for Firebase
 
 document.addEventListener("DOMContentLoaded", function () {
   const usernameInput = document.getElementById("username");
@@ -9,16 +10,17 @@ document.addEventListener("DOMContentLoaded", function () {
   loginButton.addEventListener("click", function (event) {
     event.preventDefault();
 
-    let username = usernameInput.value;
-    let password = passwordInput.value;
+    const username = usernameInput.value;
+    const password = passwordInput.value;
 
-    fetch(`${fireBaseURL}Users/${jsonEXT}`, {
-      method: "POST",
-      body: JSON.stringify(username),
-    })
-      .then((res) => {
-        if (user.password === password) {
-          // Login successful, redirect to home page
+    fetch(`${fireBaseURL}Users/${jsonEXT}`)
+      .then((response) => response.json())
+      .then((data) => {
+        const user = Object.values(data).find(
+          (user) => user.username === username
+        );
+
+        if (user && user.password === password) {
           window.location.href = "http://127.0.0.1:5500/Main-Feed/index.html";
         } else {
           alert("Incorrect username or password. Please try again.");
